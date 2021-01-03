509020bf746cd88b2ac44a005666d0825ea8c6d4
Pawel Pabianczyk <pawel.pabianczyk1999@gmail.com>
2021-01-03 17:47:29 +0100

    Fix vehicle controller address

--- src/services/vehicle.service.js
+++ src/services/vehicle.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/vehicle/";
+const API_URL = "http://localhost:8080/api/vehicle/add";
 
 const addVehicle = (brand, category, model, year, country, power, price, description, inStartDate, inExpDate, inPrice, carInStartDate, carInExpDate, carInPrice,pictureURL) =>{
     console.log(brand, category, model, year, country, power, price, description, inStartDate, inExpDate, inPrice, carInStartDate, carInExpDate, carInPrice,pictureURL);
